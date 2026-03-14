51370944129d54945e7cca9ec8e1ba05e5808089
Nick Wagner <nickwagner233@gmail.com>
2026-03-14 13:03:12 -0400

    Blueprint variation added

--- CFinals/Source/CFinals/EnvironmentActor.h
+++ CFinals/Source/CFinals/EnvironmentActor.h
@@ -10,61 +10,61 @@
 UCLASS()
 class CFINALS_API AEnvironmentActor : public AActor
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    AEnvironmentActor();
+	AEnvironmentActor();
 
 protected:
-    virtual void BeginPlay() override;
+	virtual void BeginPlay() override;
 
 public:
-    virtual void Tick(float DeltaTime) override;
+	virtual void Tick(float DeltaTime) override;
 
-    // Components
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomComponents")
-    USceneComponent* RootScene;
+	// Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomComponents")
+	USceneComponent* RootScene;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomComponents")
-    UStaticMeshComponent* MeshComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomComponents")
+	UStaticMeshComponent* MeshComp;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomComponents")
-    UPointLightComponent* PointLightComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomComponents")
+	UPointLightComponent* PointLightComp;
 
-    // Movement
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomMovement")
-    float MovementSpeed = 2.0f;
+	// Movement
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomMovement")
+	float MovementSpeed = 2.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomMovement")
-    float MovementAmplitude = 50.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomMovement")
+	float MovementAmplitude = 50.0f;
 
-    // Lighting
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomLighting")
-    FLinearColor LightColor = FLinearColor(1.0f, 1.0f, 0.0f, 1.0f);
+	// Lighting
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomLighting")
+	FLinearColor LightColor = FLinearColor(1.0f, 1.0f, 0.0f, 1.0f);
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomLighting")
-    float LightPulseSpeed = 2.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomLighting")
+	float LightPulseSpeed = 2.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomLighting")
-    float LightMinIntensity = 400.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomLighting")
+	float LightMinIntensity = 400.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomLighting")
-    float LightMaxIntensity = 2400.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomLighting")
+	float LightMaxIntensity = 2400.0f;
 
-    // Material
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomMaterial")
-    FLinearColor MaterialColor = FLinearColor(1.0f, 1.0f, 0.0f, 1.0f);
+	// Material
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomMaterial")
+	FLinearColor MaterialColor = FLinearColor(1.0f, 1.0f, 0.0f, 1.0f);
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomMaterial")
-    float GlowPulseSpeed = 2.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomMaterial")
+	float GlowPulseSpeed = 2.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomMaterial")
-    float GlowMin = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomMaterial")
+	float GlowMin = 1.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CustomMaterial")
-    float GlowMax = 8.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomMaterial")
+	float GlowMax = 8.0f;
 
 private:
-    UPROPERTY()
-    class UMaterialInstanceDynamic* DynamicMaterial;
+	UPROPERTY()
+	class UMaterialInstanceDynamic* DynamicMaterial;
 };
